2a68411fd2a708400ad5e16108456a366e9e37ba
Niket Shah <masterniket@gmail.com>
2019-11-18 12:43:52 +0000

    Add 2 column component
    Signed-off-by: Niket Shah <masterniket@gmail.com>

--- src/components/twoColumnLayout.js
+++ src/components/twoColumnLayout.js
@@ -1,23 +1,27 @@
 import React from "react"
 import SubHeading from "./subheading"
+import styled from "styled-components"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { documentToReactComponentsOptions } from './bodyContent'
+
+const TwoColumns = styled.div`
+  display: grid;
+  grid-template: auto / 1fr 1fr;
+  column-gap: 20px;
+  font-family: 'Source Sans Pro';
+  padding-left: 20px;
+  padding-right: 20px;
+`
 
 const TwoColumnLayout = info => {
-  console.log(info)
-  let options = {
-    renderNode: {
-      'embedded-asset-block': (node) => {
-        console.log(node)
-        return (<img className="img-fluid" src={/*node.data.target.fields.file["en-US"].url*/"https://www.google.com/url?sa=i&url=https%3A%2F%2Fnature.mdc.mo.gov%2Fdiscover-nature%2Ffield-guide%2Frainbow-trout&psig=AOvVaw1FR0WycKR9qFw0T3nNPGtm&ust=1572765797421000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMCZgaj_yuUCFQAAAAAdAAAAABAD"}/>)
-      }
-    }
-  }
   return (
-    <>
+    <section>
       <SubHeading name={info.heading.name} />
-      {documentToReactComponents(info.leftColumn.json, options)}
-      {documentToReactComponents(info.rightColumn.json, options)}
-    </>
+      <TwoColumns>
+        <div>{documentToReactComponents(info.leftColumn.json, documentToReactComponentsOptions)}</div>
+        <div>{documentToReactComponents(info.rightColumn.json, documentToReactComponentsOptions)}</div>
+      </TwoColumns>
+    </section>
   )
 }
 

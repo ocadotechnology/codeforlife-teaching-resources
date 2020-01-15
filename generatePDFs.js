4036a91c3fbbbbb18c57d8702c805a6fb7dcced3
Niket <niket.shah1@ocado.com>
2020-01-15 07:46:55 +0000

    make generate pdf script work again with page numbers and auth0
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- generatePDFs.js
+++ generatePDFs.js
@@ -1,23 +1,57 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer")
+require("dotenv").config({
+  path: `.env.development`,
+})
 
-async function generatePDFFromLink(page, title, link) {
-  await page.goto(link, {waitUntil: 'networkidle0', timeout: 3000}).catch(() => {})
-  await page.emulateMedia('screen')
-  await page.pdf({path: `${title}.pdf`, format: 'A4', printBackground: true})
+const sleep = milliseconds => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-(async () => {
-  const browser = await puppeteer.launch({headless: true});
-  const page = await browser.newPage();
-  await page.goto('http://localhost:9000', {waitUntil: 'networkidle2'})
+async function generatePDFFromLink(page, title, link) {
+  await page
+    .goto(link, { waitUntil: "networkidle0", timeout: 3000 })
+    .catch(() => {})
+  await page.pdf({
+    path: `${title}.pdf`,
+    displayHeaderFooter: true,
+    footerTemplate: `<p style="font-size: 10px; width: 100%; padding: 10px 50px; font-family:'Source Sans Pro'; border-top: 2px solid black;">
+        <span style="float: left;">${title}</span>
+        <span style="float: right;">www.codeforlife.education | <span class="pageNumber"></span></span>
+      </p>`,
+    format: "A4",
+    printBackground: true,
+    margin: {
+      top: 10,
+      bottom: 70,
+    },
+  })
+}
 
-  let worksheets = await page.$$eval('a', as => as.map(a => {
-    return {title: a.textContent, link: a.href} 
-  }))
+async function login(page) {
+  await page.click("#login")
+  await sleep(5000)
+  await page.type('input[name="email"]', process.env.PUPPETEER_USERNAME)
+  await page.type('input[type="password"]', process.env.PUPPETEER_PASSWORD)
+  await page.click('button[name="submit"]')
+  await sleep(3000)
+}
+
+;(async () => {
+  const browser = await puppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+  await page.goto("http://localhost:8000", { waitUntil: "networkidle2" })
+
+  await login(page)
+
+  let worksheets = await page.$$eval("a", as =>
+    as.map(a => {
+      return { title: a.textContent, link: a.href }
+    })
+  )
 
   for (let worksheet of worksheets) {
     await generatePDFFromLink(page, worksheet.title, worksheet.link)
   }
 
-  await browser.close();
-})();
+  await browser.close()
+})()

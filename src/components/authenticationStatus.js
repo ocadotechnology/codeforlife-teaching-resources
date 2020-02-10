34af62ebd2db5c74f088fcbfcb698d4bafaca404
M. Mafalda F <30693435+mariaFernando@users.noreply.github.com>
2020-02-10 12:44:10 +0000

    feat: footer page number (#6)
    * add, upgrade and remove packages
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * Use markdown for the table
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * add typename for each content model
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * use markdown for fullWidthText
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * update lockfile
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * Add code block and inline-code markdown renderers
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * Reduce BodyContent font size and use markdown for 2 column layout
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * adjust font size of code blocks
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * make generate pdf script work again with page numbers and auth0
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * Merge branch 'master' into footer_page_number
    
    * fixed padding, added margin
    
    * added puppeteer credentials mention in docs

--- src/components/authenticationStatus.js
+++ src/components/authenticationStatus.js
@@ -7,14 +7,18 @@ const AuthenticationStatus = () => {
     return (
       <>
         <h2>You are logged in and good to go!</h2>
-        <button onClick={logout}>Logout</button>
+        <button id="logout" onClick={logout}>
+          Logout
+        </button>
       </>
     )
   } else {
     return (
       <>
         <h2>Please login to access the worksheets</h2>
-        <button onClick={login}>Login with Github</button>
+        <button id="login" onClick={login}>
+          Login with Github
+        </button>
       </>
     )
   }

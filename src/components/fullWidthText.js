181d3c512e6bbd494e37f10d991d4b6be13f943e
Florian Aucomte <33633200+faucomte97@users.noreply.github.com>
2020-03-07 18:22:10 +0000

    feat: Add optional subheading to full text width component (#9)
    * Add optional subheading to full text width component

--- src/components/fullWidthText.js
+++ src/components/fullWidthText.js
@@ -1,10 +1,18 @@
 import React from "react"
+import SubHeading from "./subheading"
 import BodyContent from "./bodyContent"
 import ReactMarkdown from "react-markdown"
 import markdownRenderers from "./markdownRenderers"
 
+const renderSubheading = subheading => {
+  if (subheading) {
+    return (<SubHeading name={subheading.name} />)
+  }
+}
+
 const FullWidthText = info => {
   return (
+    {renderSubheading(info.heading)}
     <BodyContent>
       <ReactMarkdown
         source={info.content.content}

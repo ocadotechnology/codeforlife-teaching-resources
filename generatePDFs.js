5779182669c0ec3ddde8ea100529b3cf65af378d
Dioni Zhong <dioni.zhong@ocado.com>
2021-03-31 09:57:36 +0100

    fix: clean pdf naming and generate to a folder

--- generatePDFs.js
+++ generatePDFs.js
@@ -7,12 +7,14 @@ const sleep = milliseconds => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-async function generatePDFFromLink (page, title, link) {
+async function generatePDFFromLink(page, title, link) {
+  // convert title to a clean filename
+  const filename = title.trim().replace(/\s+/g, '_').toLowerCase()
   await page
     .goto(link, { waitUntil: 'networkidle0', timeout: 3000 })
-    .catch(() => {})
+    .catch(() => { })
   await page.pdf({
-    path: `${title}.pdf`,
+    path: `pdfs/${filename}.pdf`,
     displayHeaderFooter: true,
     footerTemplate: `<p style="font-size: 10px; width: 100%; padding: 10px 0px 0px; margin: 0px 50px; font-family:'Source Sans Pro'; border-top: 1px solid black;">
         <span style="float: left;">${title}</span>
@@ -27,7 +29,7 @@ async function generatePDFFromLink (page, title, link) {
   })
 }
 
-async function login (page) {
+async function login(page) {
   await page.click('#login')
   await sleep(5000)
   await page.type('input[name="email"]', process.env.PUPPETEER_USERNAME)
@@ -36,7 +38,7 @@ async function login (page) {
   await sleep(3000)
 }
 
-;(async () => {
+; (async () => {
   const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
   await page.goto('http://localhost:8000', { waitUntil: 'networkidle2' })
